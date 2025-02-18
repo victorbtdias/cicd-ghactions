0b26142d1a7b46d9dc2354e1ada2ed47131ab633
Victor <victorbtdias159@hotmail.com>
2025-02-18 20:21:21 -0300

    Teste deploy com access token para repositório privado no docker

--- src/routes.ts
+++ src/routes.ts
@@ -3,7 +3,7 @@ import express, { Request, Response } from "express";
 const router = express.Router();
 
 router.get("/", (req: Request, res: Response) => {
-  res.status(200).send("API is running");
+  res.status(200).send("API was updated again");
 });
 
 export { router };
