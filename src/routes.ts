d6cb4b47ab3153de56f696322753c3995ae127ca
Victor <victorbtdias159@hotmail.com>
2025-02-18 08:15:07 -0300

    Teste deploy

--- src/routes.ts
+++ src/routes.ts
@@ -3,7 +3,7 @@ import express, { Request, Response } from "express";
 const router = express.Router();
 
 router.get("/", (req: Request, res: Response) => {
-  res.status(200).send("API was updated :)");
+  res.status(200).send("API atualizada");
 });
 
 export { router };
