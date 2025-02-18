26b610888d0b1f6a2ac04d662baa6083870eb205
Victor <victorbtdias159@hotmail.com>
2025-02-18 19:46:00 -0300

    Teste deploy com repositório privado no docker hub

--- src/routes.ts
+++ src/routes.ts
@@ -3,7 +3,7 @@ import express, { Request, Response } from "express";
 const router = express.Router();
 
 router.get("/", (req: Request, res: Response) => {
-  res.status(200).send("API atualizada");
+  res.status(200).send("API is running");
 });
 
 export { router };
